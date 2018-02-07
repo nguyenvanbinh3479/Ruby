8793190998c64215240468b5f34e4b8deeeb7aeb
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-02-07 10:52:08 +0700

    Edited task_5

--- task_5.rb
+++ task_5.rb
@@ -1,4 +1,17 @@
 # 1. in 1 chuoi
 	# puts "hello you"
 	# print "hello you"
-# 
+# 2. tim do dai chuoi
+
+# 3. 
+	str = "hello worlddd"
+	puts "nhap ky tu can tim"
+	key = gets.chomp
+	count = 0
+	puts str.size
+	for i in 0..str.size
+		if str[i].eql? key
+			count += 1
+		end
+	end
+	puts "so lan xuat hien ky tu #{key} trong chuoi : #{count}"
