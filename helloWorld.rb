0d37a49de44f55079d1d91e0e224c567cb99e372
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-02-07 08:06:46 +0700

    Edited new method

--- helloWorld.rb
+++ helloWorld.rb
@@ -156,4 +156,16 @@ when x == 2 then "two"
 when x == 3 then "three" 
 when x == 4 then "four" 
 else "many"
+end 
+
+$global=0
+class C 
+puts "in a class #{$global}"
+def my_method
+    puts "in a method #{$global}"
+    $global=$global+1
+    $other_global=3
 end
+end
+C.new.my_method
+puts "at top-lever,$global:#{$global},$other_global: #{$other_global}"
