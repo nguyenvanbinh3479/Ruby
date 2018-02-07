4c907efa6554c12bb347d2177f7aede0edfcfec8
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-02-07 09:03:24 +0700

    Edited task1

---
+++ task_1.rb
@@ -0,0 +1,79 @@
+# 1. so sanh 2 so nguyen
+	# print "nhap so a: "
+	# a = gets
+	# a = a.to_i
+	# print "nhap so b: "
+	# b = gets
+	# b = b.to_i
+	# if a > b
+	# 	puts "#{a} > #{b}"
+	# elsif a < b
+	# 	puts "#{a} < #{b}"
+	# elsif a = b
+	# 	puts "#{a} = #{b}"
+	# end
+
+# 2. tim gia tri lon nhat (so sanh 3 so nguyen) 
+	# print "nhap so a: "
+	# a = gets
+	# a = a.to_i
+	# print "nhap so b: "
+	# b = gets
+	# b = b.to_i
+	# print "nhap so c: "
+	# c = gets
+	# c = c.to_i
+	# if a > b && a >c
+	# 	max = a 
+	# 	puts "so lon nhat la so a = #{a}"
+	# elsif b > a && b > c 
+	# 	max = b
+	# 	puts "so lon nhat la so b = #{b}"
+	# elsif c > a && c > b
+	# 	max = b
+	# 	puts "so lon nhat la so c = #{c}"
+	# end
+# 3. kiem tra so chan, le
+	# print "nhap so a: "
+	# a = gets
+	# a = a.to_i
+	# if a % 2 == 0
+	# 	puts "a = #{a} la so chan."
+	# else
+	# 	puts "a = #{a} la so le."
+	# end
+# 4. kiem tra so am duong
+	# print "nhap so a: "
+	# a = gets
+	# a = a.to_i
+	# if a >= 0
+	# 	puts "a = #{a} la so duong."
+	# else
+	# 	puts "a = #{a} la so am."
+	# end		
+# 5. kiem tra nam nhuan
+	# print "nhap nam: "
+	# a = gets
+	# a = a.to_i
+	# if a % 4 == 0 && a % 100 != 0 || a % 400 == 0
+	# 	puts "nam #{a} la nam nhuan."
+	# else
+	# 	puts "nam #{a} khong phai la nam nhuan."
+	# end
+# 6. trao doi gia tri bang su dung bien thu 3
+	# print "nhap so a: "
+	# a = gets
+	# a = a.to_i
+	# print "nhap so b: "
+	# b = gets
+	# b = b.to_i
+	# puts "so a truoc khi doi la : #{a}"
+	# puts "so b truoc khi doi la : #{b}"
+	# c = a
+	# a = b 
+	# b = c
+	# puts "so a sau khi doi la : #{a}"
+	# puts "so b sau khi doi la : #{b}"
+	
+
+	
